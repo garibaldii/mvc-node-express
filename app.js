92c920a265d61c810c169036fc27d76a47bdbe79
Matheus Garibaldi <matheus.garibaldi8@gmail.com>
2024-04-16 21:13:34 -0300

    MVC

--- app.js
+++ app.js
@@ -2,3 +2,8 @@ const express = require('express')
 const app = express()
 const loginRout = require('./routes/LoginRoute')
 
+app.use(loginRout)
+
+app.listen(3000, function(){
+    console.log(`App rodando na porta 3000`);
+})
